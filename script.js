bcc4bb9ab26262ee3b150d8e0693d853ead1e7dc
casey scott <csnz2018@hotmail.com>
2019-02-27 07:04:41 +0000

    fadeIN,fadeOut,slideToggle

--- script.js
+++ script.js
@@ -1,21 +1,35 @@
 $(document).ready(function() {
 
     //removes class makeRed, adds class makeBorder on mouseenter
-    $("button").mouseenter(function() {
+    $("#button1").mouseenter(function() {
         $(this).removeClass("makeRed").addClass("makeBorder");
     });
 
-    $("button").mouseleave(function() {
+    $("#button1").mouseleave(function() {
         $("button").removeClass("makeBorder").addClass("makeRed");
     });
 
-    $("button").click(function() {
-        $("p").slideToggle(1000);
+    $("#button2").mouseenter(function() {
+        $(this).removeClass("makeRed").addClass("makeBorder");
     });
 
-   /* $("button").click(function(){
-        $("p").hide(1000).show(1000);
-    })*/
+    $("#button2").mouseleave(function() {
+        $("button").removeClass("makeBorder").addClass("makeRed");
+    });
 
+    $("#button1").click(function() {
+        $("#para1").fadeOut(1000).fadeIn(1000)
+    })
 
+    $("#button2").click(function() {
+        $("#para2").slideToggle(1000);
+    });
+    
+    /*$("button").click(function() {
+        $("p").slideToggle(1000);
+    });*/
+
+    /*$("button").click(function() {
+        $("p").hide(9000).show(9000);
+    });*/
 });
